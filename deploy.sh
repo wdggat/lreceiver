e9f60efe8977f79fac8891a8e4ab56c3d3925cb6
刘小龙 <hzliuxiaolong@corp.netease.com>
2014-08-19 11:29:37 +0800

    add localTime in message

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,22 @@
 #!/bin/bash 
 
+#hosts=('121.40.98.72', '121.40.151.4')
+function print_usage(){
+  echo "Usage: $0 <121.40.98.72|121.40.151.4>"
+}
+
+if [[ -z $1 ]];then
+  print_usage;
+  exit;
+fi
+host=$1
+
 mvn clean package;
-scp target/lreceiver-jar-with-dependencies.jar liuo@121.40.98.72:~/lreceiver;
-ssh liuo@121.40.98.72 <<EOF
-cd lreceiver;
-./shutdown.sh;
-./startup.sh;
+scp target/lreceiver-jar-with-dependencies.jar liuo@$host:~/lreceiver;
+ssh liuo@$host <<EOF
+  cd lreceiver;
+  ./shutdown.sh;
+  ./startup.sh;
 EOF
-echo 'redeployed.'
+echo "$host receiver redeployed."
+
